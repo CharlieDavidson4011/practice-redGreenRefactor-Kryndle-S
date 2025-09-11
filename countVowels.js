a93a21c7076e5c583aa5ba6e5e7ae0ca151ae291
supernovabee <dosin.1ne@gmail.com>
2025-09-10 20:29:52 -0500

    unfinished, need the testing code

---
+++ countVowels.js
@@ -0,0 +1,14 @@
+
+
+
+function countVowels(str) {
+  let count = 0;
+  const vowels = "aeiou";  str.toLowerCase().split("").forEach(function(char) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  });  return count;
+}
+
+
+console.log(countVowels("")) ;
